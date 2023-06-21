003d958bea5dab0d766689aaf6f739f97d65c421
alanfedrizzi <alanfedrizzi@gmail.com>
2023-06-21 18:55:42 +0200

    pasta research

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-import { defineConfig, splitVendorChunkPlugin } from 'vite';
-import { resolve } from 'path';
+import { defineConfig, splitVendorChunkPlugin } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [splitVendorChunkPlugin()],
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        research: resolve(__dirname, 'src/pages/research.html')
-      }
+        main: resolve(__dirname, "index.html"),
+        research: resolve(__dirname, "src/pages/research/research.html"),
+      },
       // output: {
       //   entryFileNames: `assets/[name].js`,
       //   chunkFileNames: `assets/[name].js`,
       //   assetFileNames: `assets/[name].[ext]`
       // }
-    }
-  }
+    },
+  },
 });
