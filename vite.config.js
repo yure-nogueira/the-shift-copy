d320d7bbf78bcb441aa9879274362d4d13813007
yure-nogueira <pedro.nogueira@terceirizados.medgrupo.com.br>
2023-06-21 12:11:19 -0300

    config vite

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  plugins: [splitVendorChunkPlugin()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        research: resolve(__dirname, 'src/pages/research.html')
+      }
+      // output: {
+      //   entryFileNames: `assets/[name].js`,
+      //   chunkFileNames: `assets/[name].js`,
+      //   assetFileNames: `assets/[name].[ext]`
+      // }
+    }
+  }
+});
