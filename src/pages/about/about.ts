0f6683bc281071de029823d8e110fafd5278e282
valdir-junior10 <valdir.conceicaojr@gmail.com>
2023-06-22 16:55:54 +0100

    implmentação middle about

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -3,6 +3,7 @@ import "./../../components/cursor/cursor";
 import "./../../components/logo/logo";
 import "./../../components/menu/menu";
 import "./../../components/progress-bar/progress-bar";
+import "./../../components/perfil/perfil";
 
 // layout
 import "./../../layout/section/section";
