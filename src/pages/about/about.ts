f024329509d1994c69f50a4cb85895604ad432e7
valdir-junior10 <valdir.conceicaojr@gmail.com>
2023-06-22 14:24:18 +0100

    implementação about e componente logo

---
+++ src/pages/about/about.ts
@@ -0,0 +1,8 @@
+// components
+import "./../../components/cursor/cursor";
+import "./../../components/logo/logo";
+import "./../../components/menu/menu";
+import "./../../components/progress-bar/progress-bar";
+
+// layout
+import "./../../layout/section/section";
