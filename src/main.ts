07749a897e6280c6005912fbf90d3c4c4c7ff213
yure-nogueira <pedro.nogueira@terceirizados.medgrupo.com.br>
2023-06-23 11:20:26 -0300

    animação logo slider

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,14 @@ import './components/cursor/cursor';
 import './components/menu/menu';
 import './components/progress-bar/progress-bar';
 import './components/forward-slider/forward-slider';
+import './components/logo/logo';
 
 // layout
 import './layout/section/section';
 
 // pages
-import "./pages/about/about";
-import "./pages/research/research";
+import './pages/about/about';
+import './pages/research/research';
 
 // utilities
 import './utilities/animation-text';
