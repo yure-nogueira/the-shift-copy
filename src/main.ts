f973e6722fff85d1c2be022d5fb04463bf234342
alanfedrizzi <alanfedrizzi@gmail.com>
2023-06-26 19:22:09 +0200

    animation-title adicionado no main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,21 @@
 // components
-import './components/carousel/carousel';
+import "./components/carousel/carousel";
 // import "./components/cursor/cursor";
-import './components/menu/menu';
-import './components/progress-bar/progress-bar';
-import './components/forward-slider/forward-slider';
-import './components/logo/logo';
-import './components/shell-button/shell-button';
+import "./components/menu/menu";
+import "./components/progress-bar/progress-bar";
+import "./components/forward-slider/forward-slider";
+import "./components/logo/logo";
+import "./components/shell-button/shell-button";
 
 // layout
-import './layout/intro/intro';
-import './layout/section/section';
+import "./layout/intro/intro";
+import "./layout/section/section";
 
 // pages
-import './pages/about/about';
-import './pages/research/research';
-import './pages/index/index';
+import "./pages/about/about";
+import "./pages/research/research";
+import "./pages/index/index";
 
 // utilities
-import './utilities/animation-text';
+import "./utilities/animation-text";
+import "./utilities/animation-title";
