f71838bc4c2808864a575a60fecb9edc5c354e75
valdir-junior10 <valdir.conceicaojr@gmail.com>
2023-06-26 21:22:08 +0100

    ajuste escopo carousel

--- src/components/carousel/carousel.ts
+++ src/components/carousel/carousel.ts
@@ -3,26 +3,30 @@ import { gsap } from "gsap";
 import { horizontalLoop } from "./../../utilities/gsap/helper-functions/helper-functions";
 import { DOM } from "./../../utilities/constants";
 
-const forward = gsap.utils.toArray(`[animation=${DOM.carouselForward}]`);
-const backward = gsap.utils.toArray(`[animation=${DOM.carouselBackward}]`);
-
-forward.forEach((carousel) => {
-  const carouselSpan = gsap.utils.toArray(
-    `[animation=${DOM.carouselForwardSpan}]`,
-    carousel as HTMLElement
-  );
-  horizontalLoop(carouselSpan, {
-    repeat: -1,
+(() => {
+  const forward = gsap.utils.toArray(`[animation=${DOM.carouselForward}]`);
+  const backward = gsap.utils.toArray(`[animation=${DOM.carouselBackward}]`);
+  
+  forward.forEach((carousel) => {
+    const carouselSpan = gsap.utils.toArray(
+      `[animation=${DOM.carouselForwardSpan}]`,
+      carousel as HTMLElement
+    );
+    horizontalLoop(carouselSpan, {
+      repeat: -1,
+    });
   });
-});
-
-backward.forEach((carousel) => {
-  const carouselSpan = gsap.utils.toArray(
-    `[animation=${DOM.carouselBackwardSpan}]`,
-    carousel as HTMLElement
-  );
-  horizontalLoop(carouselSpan, {
-    reversed: true,
-    repeat: -1,
+  
+  backward.forEach((carousel) => {
+    const carouselSpan = gsap.utils.toArray(
+      `[animation=${DOM.carouselBackwardSpan}]`,
+      carousel as HTMLElement
+    );
+    horizontalLoop(carouselSpan, {
+      reversed: true,
+      repeat: -1,
+    });
   });
-});
+})()
+
+
